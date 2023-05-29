18d30ceac9bb9573d8b892c77531fc8c7ad58c7b
EnzoGaticaOdato <enzogaticaodato@gmail.com>
2023-05-29 11:10:25 -0700

    Routing listo

--- src/app/components/logoylogin/logoylogin.component.ts
+++ src/app/components/logoylogin/logoylogin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logoylogin',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./logoylogin.component.css']
 })
 export class LogoyloginComponent {
+  constructor(private router: Router){
 
+  }
+
+  login(){
+    this.router.navigate(['/login'])
+  }
 }
